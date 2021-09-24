a56cc5c21376b7594e3e9f7af002347e5c9f6004
Rahul Roshan <tecnokumar8@gmail.com>
2021-09-24 21:53:54 +0500

    update settings.py

--- APiPro/settings.py
+++ APiPro/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'hw**bwzgk%tk377383%9&cs&i%i#86z1+yb()2-&^ihp_cmpz7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','fyp-project1.herokuapp.com/']
+
 
 
 # Application definition
